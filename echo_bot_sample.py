f4107535ecd7520882762ce1aa12f394d2e4c78a
Daniil Yakovlev <ddyakovlev@yandex.ru>
2019-12-16 08:53:38 +0000

    Add buttons

--- echo_bot_sample.py
+++ echo_bot_sample.py
@@ -1,24 +1,30 @@
-import os
-import telebot
 import config
+import telebot
 
+bot = telebot.TeleBot(config.token)
 
-bot = telebot.TeleBot(config.token)#os.environ['BOT_TOKEN'])
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('Привет', 'Пока')
 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'пыня':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello, I'm simple echo bot. Tell me something!")
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
 
-
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
+def main():    
+    bot.polling()
 
 if __name__ == '__main__':
-    bot.infinity_polling()
+    main()
