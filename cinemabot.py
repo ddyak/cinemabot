5712b986d84c7cc8b3472821eda68824f192be63
Daniil Yakovlev <ddyakovlev@yandex.ru>
2019-12-22 21:42:06 +0700

    Add token as env var; add scrap lib.

---
+++ cinemabot.py
@@ -0,0 +1,36 @@
+import telebot
+import config
+import cinemabot_lib.scrapper
+
+bot = telebot.TeleBot(config.token)
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    start_message = 'Cinemabot v1.0:\n' \
+                     '/help - show commands and usage'
+    bot.send_message(message.chat.id, start_message)
+
+
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    help_message = 'commands:\n'\
+                   '/help\n' \
+                   '/start\n' \
+                   '[$YOUR_MOVIE]'
+    bot.send_message(message.chat.id, help_message)
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    desc, url = cinemabot_lib.scrapper.search_movie(message.text)
+    bot.send_message(message.chat.id, desc)
+    bot.send_photo(chat_id=message.chat.id, photo=url)
+
+
+def main():    
+    bot.polling()
+
+
+if __name__ == '__main__':
+    main()
---
+++ cinemabot_lib/scrapper.py
@@ -0,0 +1,36 @@
+import urllib
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+from imdb import IMDb
+import re
+
+ia = IMDb()
+
+
+def search_movie(request):
+    movies = ia.search_movie(request)
+    movie = movies[0]
+    ia.update(movie, info=['main', 'plot', 'taglines', 'vote details'])
+
+    description = movie.get('title') + '\nScore: ' \
+                  + str(movie.get('arithmetic mean')) + '\n' \
+                  + re.split(r'::', movie.get('plot')[0], maxsplit=1)[0]
+    cover_url = movie['full-size cover url']
+
+    # text = urllib.parse.quote_plus(message.text + 'смотреть онлайн')
+    # url = 'https://google.com/search?q=' + text
+
+    # response = requests.get(url)
+
+    # soup = BeautifulSoup(response.text, 'lxml')
+    # urls = []
+    # for g in soup.find_all("div", class_="kCrYT"):
+    #    for t in g.find_all("a", href=True):
+    #        if t['href'][:7] != '/search':
+    #            urls.append(t['href'][7:])
+
+    # bot.send_message(message.chat.id, '\n'.join(urls[:5]))
+
+
+    return description, cover_url
