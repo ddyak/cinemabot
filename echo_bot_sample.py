d3e59a07b7700522b185b0d5c2d95d0673c0f34e
Daniil Yakovlev <ddyakovlev@yandex.ru>
2019-12-22 08:13:52 +0000

    Simple cinemabot.

--- echo_bot_sample.py
+++ echo_bot_sample.py
@@ -10,14 +10,37 @@ keyboard1.row('Привет', 'Пока')
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
 
+import urllib
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+from imdb import IMDb
+import re
+
+ia = IMDb()
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'пыня':
-        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+    movies = ia.search_movie(message.text)
+    movie = movies[0]
+    ia.update(movie, info=['main', 'plot', 'taglines', 'vote details'])
+
+    #text = urllib.parse.quote_plus(message.text + 'смотреть онлайн')
+    #url = 'https://google.com/search?q=' + text
+
+    #response = requests.get(url)
+
+    #soup = BeautifulSoup(response.text, 'lxml')
+    #urls = []
+    #for g in soup.find_all("div", class_="kCrYT"):
+    #    for t in g.find_all("a", href=True):
+    #        if t['href'][:7] != '/search':
+    #            urls.append(t['href'][7:])
+
+   # bot.send_message(message.chat.id, '\n'.join(urls[:5]))
+    bot.send_message(message.chat.id, movie.get('title') + ' ' + str(movie.get('arithmetic mean')))
+    bot.send_message(message.chat.id, re.split(r'::', movie.get('plot')[0],maxsplit=1)[0])
+    bot.send_photo(chat_id=message.chat.id, photo=movie['full-size cover url'])
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
